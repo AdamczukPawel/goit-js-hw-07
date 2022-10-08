0e1e393a8259151d523b2642bdce841ab674fc51
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-08 22:59:38 +0200

    Wykonanie 1 zadania z pracy domowej

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
+const galleryPictures = document.querySelector('div.gallery');
+const newGalery = galleryItems
+    .map(({preview, original, description}) => 
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img 
+            class="gallery__image" 
+            src="${preview}" 
+            data-source="${original}"
+            alt="${description}" 
+          />
+        </a>
+      </div>`)
+    .join('');
+    
+galleryPictures.insertAdjacentHTML("afterbegin", newGalery);
+ 
+galleryPictures.addEventListener("click", event => {
+  event.preventDefault();
+  if (event.target.nodeName !== `IMG`) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `); 
+  instance.show();
+  document.addEventListener('keydown', (ev) => {
+    if (ev.code === "Escape") {
+      instance.close();
+    }
+  });
+});
 console.log(galleryItems);
+
